c6a0fcfa6608c12e88ebc9049420579cff98a085
Thijs <t.arkema@ultraware.nl>
2018-08-21 15:16:44 +0200

    typescript config and file rename

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,26 @@ const path = require('path')
 
 module.exports = {
   mode: 'production',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        include: [path.resolve(__dirname, './src')],
-      },
-    ],
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.js$/,
+  //       loader: 'babel-loader',
+  //       include: [path.resolve(__dirname, './src')],
+  //     },
+  //   ],
+  // },
+  // resolve: {
+  //   extensions: ['.js'],
+  //   alias: {
+  //     src: path.resolve(__dirname, './src'),
+  //   },
+  // },
+  externals: {
+    'js-md5': 'js-md5',
+    vuex: 'vuex',
   },
-  resolve: {
-    extensions: ['.js'],
-    alias: {
-      src: path.resolve(__dirname, './src'),
-    },
-  },
-  entry: path.resolve(__dirname, './src/index.js'),
+  entry: path.resolve(__dirname, './lib/index.js'),
   output: {
     path: path.resolve(__dirname, './dist/'),
     filename: 'index.js',
