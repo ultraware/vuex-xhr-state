bbc05ae271165749bfb9da71d4b80f7cab4a13a6
Rick <r.aikema@ultraware.nl>
2018-12-20 11:19:54 +0100

    Return pending promise when call is pending

--- src/actions.ts
+++ src/actions.ts
@@ -9,7 +9,8 @@ export default <D, P, S, RS>(
   method: VxsMethod<P, D>,
   inValidateGroup: () => void,
 ): VxsActionTree<S, RS, P, D> => {
-  const result = <VxsActionTree<S, RS, P, D>> {
+  let fetchPromise: Promise<IVxsResponse<D>> | null = null
+  const result = <VxsActionTree<S, RS, P, D>>{
     [ACTION.SEND](store: ActionContext<VxsExtendedState<D, S>, RS>, payload: P): Promise<IVxsResponse<D>> {
       const methodPromise = runMethod(result.method, store, payload)
       methodPromise
@@ -19,12 +20,19 @@ export default <D, P, S, RS>(
 
     [ACTION.FETCH](store: ActionContext<VxsExtendedState<D, S>, RS>, payload: P): Promise<IVxsResponse<D>> {
       if (cache && store.getters[GET.PENDING](payload) && store.getters[GET.PENDING](payload)) {
-        return getResolvingPromiseFromGetter(store, payload)
+        if (!fetchPromise) {
+          return getResolvingPromiseFromGetter(store, payload)
+        }
+        return fetchPromise
       }
       if (cache && store.getters[GET.FETCHED](payload) && store.getters[GET.FETCHED](payload)) {
         return getResolvingPromiseFromGetter(store, payload)
       }
-      return runMethod(result.method, store, payload)
+      fetchPromise = runMethod(result.method, store, payload)
+      fetchPromise.finally(() => {
+        fetchPromise = null
+      })
+      return fetchPromise
     },
 
     [ACTION.RESET](store: ActionContext<VxsExtendedState<D, S>, RS>): void {
@@ -40,12 +48,12 @@ export default <D, P, S, RS>(
     }
     result[ACTION.INVALIDATE] = (store: ActionContext<VxsExtendedState<D, S>, RS>, payload: P): void => {
       const key = payloadToKey(payload)
-      store.commit(MUTATIONS.INVALIDATE, {key})
+      store.commit(MUTATIONS.INVALIDATE, { key })
     }
 
     result[ACTION.INVALIDATE_ALL] = (store: ActionContext<VxsExtendedState<D, S>, RS>): void => {
       const payloadKeys = store.getters[GET.PAYLOAD_KEYS]()
-      payloadKeys.forEach((key: string) => store.commit(MUTATIONS.INVALIDATE, {key}))
+      payloadKeys.forEach((key: string) => store.commit(MUTATIONS.INVALIDATE, { key }))
     }
   }
   result.method = method
@@ -63,14 +71,14 @@ export default <D, P, S, RS>(
 
     // @ts-ignore
     if (reply.then) {
-      prom = <Promise<IVxsResponse<D>>> reply
+      prom = <Promise<IVxsResponse<D>>>reply
       prom.then((response: IVxsResponse<D>): void => {
         mutateReceived(store, key, uniqueId, response)
       }).catch((response): void => {
         mutateFailed(store, key, uniqueId, response)
       })
     } else {
-      const data = <D> reply
+      const data = <D>reply
       prom = getResolvingPromise({
         data,
       })
@@ -78,17 +86,18 @@ export default <D, P, S, RS>(
         data,
       })
     }
-    store.commit(MUTATIONS.REQUEST, {key})
-    store.dispatch(`${GLOBAL_NAMESPACE}/${GLOBAL_ACTIONS.REQUEST}`, uniqueId, {root: true})
+    store.commit(MUTATIONS.REQUEST, { key })
+    store.dispatch(`${GLOBAL_NAMESPACE}/${GLOBAL_ACTIONS.REQUEST}`, uniqueId, { root: true })
     return prom
   }
 
   function mutateReceived(
     store: ActionContext<VxsExtendedState<D, S>, RS>,
     key: string, uniqueId: number,
-    response: IVxsResponse<D>): void {
-    store.commit(MUTATIONS.RECEIVED, {key, response})
-    store.dispatch(`${GLOBAL_NAMESPACE}/${GLOBAL_ACTIONS.RECEIVED}`, uniqueId, {root: true})
+    response: IVxsResponse<D>,
+  ): void {
+    store.commit(MUTATIONS.RECEIVED, { key, response })
+    store.dispatch(`${GLOBAL_NAMESPACE}/${GLOBAL_ACTIONS.RECEIVED}`, uniqueId, { root: true })
   }
 
   function mutateFailed(
@@ -97,10 +106,10 @@ export default <D, P, S, RS>(
     uniqueId: number,
     response: IVxsResponse<D>,
   ): void {
-    store.commit(MUTATIONS.FAILED, {key, response})
+    store.commit(MUTATIONS.FAILED, { key, response })
     store.dispatch(
       `${GLOBAL_NAMESPACE}/${GLOBAL_ACTIONS.FAILED}`,
-      {key: uniqueId, response}, {root: true},
+      { key: uniqueId, response }, { root: true },
     )
   }
 
