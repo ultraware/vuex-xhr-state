4529de71e0b0a46a515ecfea5c59175e10d922bc
thijs <thijsarkema@hotmail.com>
2019-01-31 12:30:18 +0100

    fix response type PUT and POST

--- src/VuexXhrPut.ts
+++ src/VuexXhrPut.ts
@@ -1,10 +1,10 @@
 import { IVxsOptions } from './types'
 import VuexXhr from './VuexXhr'
 
-export class VuexXhrPut<P, S = object, RS = object> extends VuexXhr<S, RS, P, undefined> {
+export class VuexXhrPut<P, S = object, RS = object, D = undefined> extends VuexXhr<S, RS, P, D> {
   // @todo the pending prop uses the payload so for every put there is a pending in the state
   // @todo function fetch is not correct when we do put
-  constructor(options: IVxsOptions<undefined, P, S >) {
+  constructor(options: IVxsOptions<D, P, S >) {
     options = options || {}
     options.cache = false
     super(options)
