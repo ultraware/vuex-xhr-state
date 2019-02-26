96365af2ab79daf9dc97a572e38e961574c2060d
Piet de Vries <piet@compenda.nl>
2019-02-26 13:25:34 +0100

    remove errorHandler from md5

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,8 +1,23 @@
 import md5 from 'js-md5'
+import { IVxsPayload } from './types'
 
 export function payloadToKey(payload: unknown): string {
   if (payload === undefined) {
     return md5(JSON.stringify('undefined'))
   }
+
+  if (typeof payload === 'object') {
+    const keys = Object.keys(<IVxsPayload>payload)
+
+    if (keys.includes('errorHandler')) {
+      if (keys.length === 1) {
+        return md5(JSON.stringify('undefined'))
+      }
+
+      payload = JSON.parse(JSON.stringify(payload))
+      delete (<IVxsPayload> payload).errorHandler
+    }
+  }
+
   return md5(JSON.stringify(payload))
 }
